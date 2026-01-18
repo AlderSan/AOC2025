ce3dafeff4b7d0242a728e3208dc60560bc17874
AlderSan <gentr023@gmail.com>
2026-01-17 23:16:49 -0600

    Complete q9 part 2

--- Day 9/main.py
+++ Day 9/main.py
@@ -1,7 +1,9 @@
 from input import puzzleinput
 #from testinput import puzzleinput
+from shapely.geometry import Polygon
+from shapely import covered_by
 
-question_part = 1
+question_part = 2
 
 def calc_area(corner1: tuple[int, int], corner2: tuple[int, int]) -> int:
     x1, y1 = corner1
@@ -25,9 +27,34 @@ def find_largest_area(list_of_corners: list[tuple[int, int]]) -> int:
                 largest_area_so_far = area
     return largest_area_so_far
 
+def opposite_corners(corner1: tuple[int, int], corner2: tuple[int, int]) -> tuple[tuple[int, int], tuple[int, int]]:
+    x1, y1 = corner1
+    x2, y2 = corner2
+    corner3 = (x1, y2)
+    corner4 = (x2, y1)
+    return corner3, corner4
+
+def find_largest_in_polygon(list_of_corners: list[tuple[int, int]], polygon: Polygon) -> int:
+    largest_area_so_far = 0
+    for corner1 in list_of_corners:
+        for corner2 in list_of_corners:
+            corner3, corner4 = opposite_corners(corner1, corner2)
+            rectangle = Polygon([corner1, corner3, corner2, corner4])
+            if covered_by(rectangle, polygon):
+                area = calc_area(corner1, corner2)
+                if area > largest_area_so_far:
+                    largest_area_so_far = area
+    return largest_area_so_far
+
+
 def main():
     red_tiles_list = input_to_corners(puzzleinput)
-    result = find_largest_area(red_tiles_list)
+    result = 0
+    if question_part == 1:
+        result = find_largest_area(red_tiles_list)
+    if question_part == 2:
+        polygon = Polygon(red_tiles_list)
+        result = find_largest_in_polygon(red_tiles_list, polygon)
     print("Answer: ", result)
 
 if __name__ == "__main__":
